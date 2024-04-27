cca0628158968bbbba853851e4725032ed712144
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-04-27 22:02:01 +0700

    feat: init sidebar

---
+++ app/(platform)/(dashboard)/_components/Nav-item.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+
+export type Organization = {
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
+
+interface NavItemProps {
+  isExpanded: boolean;
+  isActive: boolean;
+  organization: Organization;
+  onExpand: (id: string) => void;
+}
+
+const NavItem = ({
+  isExpanded,
+  isActive,
+  organization,
+  onExpand,
+}: NavItemProps) => {
+  return <div>Nav-item</div>;
+};
+
+export default NavItem;
