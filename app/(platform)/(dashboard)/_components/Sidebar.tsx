42d3e206c1eb54f6f1f4ae81236d5748b91dc563
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-04-27 23:47:20 +0700

    feat: done create for sidebar

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import React from "react";
 import { Accordion } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 import { useLocalStorage } from "usehooks-ts";
 import NavItem, { Organization } from "./Nav-item";
 
@@ -50,7 +50,15 @@ const Sidebar = ({ storageKey = "t-sidebar-state" }: SideBarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
@@ -76,15 +84,17 @@ const Sidebar = ({ storageKey = "t-sidebar-state" }: SideBarProps) => {
         defaultValue={defaultAccordionValue}
         className="space-y-2"
       >
-        {userMemberships.data.map(({ organization }) => (
-          <NavItem
-            key={organization.id}
-            isActive={activeOrganization?.id === organization.id}
-            isExpanded={expanded[organization.id]}
-            organization={organization as Organization}
-            onExpand={onExpand}
-          />
-        ))}
+        {userMemberships.data
+          .map(({ organization }) => (
+            <NavItem
+              key={organization.id}
+              isActive={activeOrganization?.id === organization.id}
+              isExpanded={expanded[organization.id]}
+              organization={organization as Organization}
+              onExpand={onExpand}
+            />
+          ))
+          .toReversed()}
       </Accordion>
     </>
   );
