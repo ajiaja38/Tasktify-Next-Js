a2c85ee91e0c8c0361085c4f12e7f239d20b2a52
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-04-28 14:03:27 +0700

    feat: Update init Prisma ✅, add and get list of board ✨🎉

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,21 @@
+import { db } from "@/lib/db";
 import React from "react";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const OrganizationIdPage = () => {
-  return <div>organization page</div>;
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} {...board} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
